67883f19af01d1b61479c1ce085015afd4763005
donus1perdonus <ekzozeta@gmail.com>
2025-06-23 23:32:36 +0300

    upd: add task5

---
+++ tasks/task5.py
@@ -0,0 +1,48 @@
+from utils import Graph
+from collections import deque
+import sys
+
+
+def is_bipartite(graph: Graph):
+    n = graph.size()
+    color = [-1] * n  # 0-индексация
+    part1, part2 = [], []
+    
+    for start in range(n):  # от 0 до n-1
+        if color[start] == -1:
+            queue = deque([start])
+            color[start] = 0
+            part1.append(start)
+            
+            while queue:
+                v = queue.popleft()
+                for u in graph.adjacency_list(v + 1):  # +1 для перехода к 1-индексации
+                    u = u - 1  # переводим обратно в 0-индексацию
+                    if color[u] == -1:
+                        color[u] = 1 - color[v]
+                        if color[u] == 0:
+                            part1.append(u)
+                        else:
+                            part2.append(u)
+                        queue.append(u)
+                    elif color[u] == color[v]:
+                        return False, [], []
+    
+    return True, sorted(part1), sorted(part2)
+
+
+def solve_task(graph: Graph, **kwargs) -> str:
+    if graph.size() > 1000:
+        sys.setrecursionlimit(max(sys.getrecursionlimit(), graph.size() * 2))
+    
+    bipartite, part1, part2 = is_bipartite(graph)
+    
+    if bipartite:
+        result = []
+        result.append("First set:")
+        result.append(f"\t{{{', '.join(map(str, part1))}}},")
+        result.append("Second set:")
+        result.append(f"\t{{{', '.join(map(str, part2))}}}.")
+        return "\n".join(result)
+    else:
+        return "Graph is not bipartite." 
